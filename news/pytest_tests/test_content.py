d851641f10bae5752ca891eaadb9e6370d546d5d
nikita-git <nikita.maximov@gmail.com>
2024-08-12 19:59:42 +0300

    Добавил тест сортировки новостей на странице.

--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -18,3 +18,14 @@ def test_news_count(many_news, client):
     object_list = response.context['object_list']
     news_count = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
+
+
+@pytest.mark.django_db
+def test_news_order(many_news, client):
+    """Тест сортировки новостей."""
+    url = reverse('news:home')
+    response = client.get(url)
+    object_list = response.context['object_list']
+    all_dates = [news.date for news in object_list]
+    sorted_dates = sorted(all_dates, reverse=True)
+    assert all_dates == sorted_dates
