702e785bc5d74d0dde61d50fd59ba8e5e914c25a
nikita-git <nikita.maximov@gmail.com>
2024-08-13 08:10:20 +0300

    Реализованы тесты по заданию.

--- news/pytest_tests/test_content.py
+++ news/pytest_tests/test_content.py
@@ -1,13 +1,10 @@
 """Тест контента."""
-# - Комментарии на странице отдельной новости отсортированы в
-# хронологическом порядке: старые в начале списка, новые — в конце.
-# - Анонимному пользователю недоступна форма для отправки комментария
-# на странице отдельной новости, а авторизованному доступна.
 import pytest
-from pytest_django.asserts import assertInHTML
 
 from django.conf import settings
 from django.urls import reverse
+
+from news.forms import CommentForm
 
 
 @pytest.mark.django_db
@@ -36,17 +33,25 @@ def test_comments_order(news, client):
     """Тест сортировки комментариев."""
     url = reverse('news:detail', args=(news.id,))
     response = client.get(url)
-    # Проверяем, что объект новости находится в словаре контекста
-    # под ожидаемым именем - названием модели.
     assert 'news' in response.context
-    # Получаем объект новости.
     news = response.context['news']
-    # Получаем все комментарии к новости.
     all_comments = news.comment_set.all()
     print(f'{all_comments}')
-    # Собираем временные метки всех новостей.
     all_timestamps = [comment.created for comment in all_comments]
-    # Сортируем временные метки, менять порядок сортировки не надо.
     sorted_timestamps = sorted(all_timestamps)
-    # Проверяем, что временные метки отсортированы правильно.
     assert all_timestamps == sorted_timestamps
+
+
+@pytest.mark.parametrize(
+    'name',
+    (
+        ('news:detail'),
+        ('news:edit')
+    )
+)
+def test_pages_contains_form(author_client, name, comment):
+    """Проверка доступности форм."""
+    url = reverse(name, args=('1',))
+    response = author_client.get(url)
+    assert 'form' in response.context
+    assert isinstance(response.context['form'], CommentForm)
